63b9988299362408da9cf60c8ff3658ef9325665
EnZon3 <37979038+EnZon3@users.noreply.github.com>
2024-02-01 13:16:00 -0500

    removed this stupid error

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.pcss';
 	import { dev } from '$app/environment';
+	// @ts-ignore
 	import { inject } from '@vercel/analytics';
 
 	inject({ mode: dev ? 'development' : 'production' });
