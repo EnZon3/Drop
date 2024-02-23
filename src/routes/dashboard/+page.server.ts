e7c33011915f51eb7daf413e3bd8569ae286afa1
EnZon3 <37979038+EnZon3@users.noreply.github.com>
2024-02-23 10:23:32 -0500

    Modified onboarding architecture

---
+++ src/routes/dashboard/+page.server.ts
@@ -0,0 +1,20 @@
+import { redirect } from '@sveltejs/kit';
+import db from '$lib/db';
+
+export async function load({ locals: { supabase, getSession } }) {
+	try {
+		const session = await getSession();
+		const username = session?.user.user_metadata.username;
+
+		let { data, error } = await supabase
+			.from('profiles')
+			.select('*')
+			.eq('username', username);
+
+		if (!data) throw redirect(303, '/dashboard/onboarding');
+        
+	} catch (err) {
+		console.log(err);
+		throw redirect(303, '/dashboard/onboarding');
+	}
+}
